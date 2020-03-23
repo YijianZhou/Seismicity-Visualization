8a5fddc2dbcb28885022153b0af2728d6d47981e
Yijian ZHOU <zhouyj.pku@gmail.com>
2020-03-23 17:12:28 +0800

    Add files via upload

--- plot_b_map.py
+++ plot_b_map.py
@@ -1,5 +1,6 @@
 """ Plot b mapping
 """
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 import multiprocessing as mp
@@ -18,14 +19,17 @@ lon_rng = cfg.lon_rng
 xy_grid = cfg.xy_grid
 lat = np.arange(lat_rng[0], lat_rng[1], xy_grid)
 lon = np.arange(lon_rng[0], lon_rng[1], xy_grid)
-print(lat)
-print(lon)
+print('lat range:', lat[0],lat[-1])
+print('lon range:', lon[0],lon[-1])
 b_mat = -np.ones([len(lat), len(lon)])
+mc_mat = -np.ones([len(lat), len(lon)])
 slice_radius = cfg.slice_radius
 min_num = cfg.min_num
+max_b = cfg.max_b
+min_mc = cfg.min_mc
 num_proc = cfg.num_proc
 # plot config
-fig_fname = cfg.fig_fname
+fig_root, fig_fname = os.path.split(cfg.fig_fname)
 fig_title = cfg.fig_title
 fig_size = cfg.fig_size
 fsize_label = cfg.fsize_label
@@ -34,22 +38,25 @@ mag_corr = cfg.mag_corr
 mark_size = cfg.mark_size
 cmap = plt.get_cmap(cfg.cmap)
 alpha = cfg.alpha
+cbar_frac = cfg.cbar_frac
+cbar_asp = cfg.cbar_asp
+line_wid = cfg.line_wid
 # read & filter catalog
 events = read_ctlg(ctlg_path)
 events = slice_ctlg(events, lat_rng=lat_rng, lon_rng=lon_rng)
 mag = (np.array(list(events['mag'])) + mag_corr) * mark_size
 faults = read_fault(fault_path, lat_rng, lon_rng)
 
-def calc_b(lati, loni):
+def slice_calc_mc_b(lati, loni):
     eventsi = slice_ctlg_circle(events, lati, loni, slice_radius)
     mc, [b_val, b_dev] = calc_mc_b(eventsi['mag'], min_num=min_num)
-    return b_val
+    return mc, b_val
 
-# calc b map
+# calc mc & b map
 if num_proc==1:
     for i,lati in enumerate(lat):
       for j,loni in enumerate(lon):
-        b_mat[i,j] = calc_b(lati, loni)
+        mc_mat[i,j], b_mat[i,j] = slice_calc_mc_b(lati, loni)
 else:
     pool = mp.Pool(num_proc)
     out=[]
@@ -60,7 +67,12 @@ else:
     pool.join()
     for outi in out:
         i, j = outi[0:2]
-        b_mat[i,j] = outi[-1].get()
+        mc_mat[i,j], b_mat[i,j] = outi[2:].get()
+# drop bad b
+cond_b = b_mat>max_b
+cond_mc = mc_mat<min_mc
+b_mat[cond_b+cond_mc] = np.nan
+mc_mat[cond_b+cond_mc] = np.nan
 
 # plot b mapping
 fig = plt.figure(figsize=fig_size)
@@ -68,15 +80,33 @@ ax = plt.gca()
 im = plt.imshow(b_mat, cmap=cmap, origin='lower', 
                 extent=[lon[0]-xy_grid/2, lon[-1]+xy_grid/2,
                         lat[0]-xy_grid/2, lat[-1]+xy_grid/2])
-cbar = plt.colorbar(im, ax=ax, fraction=0.05)
+cbar = plt.colorbar(im, ax=ax, fraction=cbar_frac, aspect=cbar_asp)
 # plot faults and events
-for fault in faults: plt.plot(fault[:,0], fault[:,1], color='k', linewidth=1)
+for fault in faults: plt.plot(fault[:,0], fault[:,1], color='k', linewidth=line_wid)
 plt.scatter(events['lon'], events['lat'], mag, color='gray', alpha=alpha)
 # tune fig
 cbar.ax.set_ylabel('b-value', fontsize=fsize_label)
 plt.setp(ax.xaxis.get_majorticklabels(), fontsize=fsize_label)
 plt.setp(ax.yaxis.get_majorticklabels(), fontsize=fsize_label)
-plt.title(fig_title, fontsize=fsize_title)
+plt.title('b-Value Map: %s'%fig_title, fontsize=fsize_title)
 plt.tight_layout()
-plt.savefig(fig_fname)
+plt.savefig('%s/b_%s'%(fig_root, fig_fname))
+
+# plot b mapping
+fig = plt.figure(figsize=fig_size)
+ax = plt.gca()
+im = plt.imshow(mc_mat, cmap=cmap, origin='lower',
+                extent=[lon[0]-xy_grid/2, lon[-1]+xy_grid/2,
+                        lat[0]-xy_grid/2, lat[-1]+xy_grid/2])
+cbar = plt.colorbar(im, ax=ax, fraction=cbar_frac, aspect=cbar_asp)
+# plot faults and events
+for fault in faults: plt.plot(fault[:,0], fault[:,1], color='k', linewidth=line_wid)
+plt.scatter(events['lon'], events['lat'], mag, color='gray', alpha=alpha)
+# tune fig
+cbar.ax.set_ylabel('Mc', fontsize=fsize_label)
+plt.setp(ax.xaxis.get_majorticklabels(), fontsize=fsize_label)
+plt.setp(ax.yaxis.get_majorticklabels(), fontsize=fsize_label)
+plt.title('Mc Map: %s'%fig_title, fontsize=fsize_title)
+plt.tight_layout()
+plt.savefig('%s/mc_%s'%(fig_root, fig_fname))
 plt.show()
