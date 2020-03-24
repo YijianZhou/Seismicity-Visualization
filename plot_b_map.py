97d790e68ee254c5ad3ffe60839ed9fd593b2a91
Yijian ZHOU <zhouyj.pku@gmail.com>
2020-03-24 11:48:12 +0800

    Add files via upload

--- plot_b_map.py
+++ plot_b_map.py
@@ -22,6 +22,8 @@ lon = np.arange(lon_rng[0], lon_rng[1], xy_grid)
 print('lat range:', lat[0],lat[-1])
 print('lon range:', lon[0],lon[-1])
 b_mat = -np.ones([len(lat), len(lon)])
+db_mat = -np.ones([len(lat), len(lon)])
+a_mat = -np.ones([len(lat), len(lon)])
 mc_mat = -np.ones([len(lat), len(lon)])
 slice_radius = cfg.slice_radius
 min_num = cfg.min_num
@@ -40,6 +42,7 @@ cmap = plt.get_cmap(cfg.cmap)
 alpha = cfg.alpha
 cbar_frac = cfg.cbar_frac
 cbar_asp = cfg.cbar_asp
+cbar_pad = cfg.cbar_pad
 line_wid = cfg.line_wid
 b_rng = cfg.b_rng
 mc_rng = cfg.mc_rng
@@ -52,13 +55,14 @@ faults = read_fault(fault_path, lat_rng, lon_rng)
 def slice_calc_mc_b(lati, loni):
     eventsi = slice_ctlg_circle(events, lati, loni, slice_radius)
     mc, [b_val, b_dev] = calc_mc_b(eventsi['mag'], min_num=min_num)
-    return mc, b_val
+    a_val = len(eventsi) if len(eventsi)>min_num else np.nan
+    return mc, b_val, b_dev, a_val
 
 # calc mc & b map
 if num_proc==1:
     for i,lati in enumerate(lat):
       for j,loni in enumerate(lon):
-        mc_mat[i,j], b_mat[i,j] = slice_calc_mc_b(lati, loni)
+        mc_mat[i,j], b_mat[i,j], db_mat[i,j], a_mat[i,j] = slice_calc_mc_b(lati, loni)
 else:
     pool = mp.Pool(num_proc)
     out=[]
@@ -75,40 +79,39 @@ cond_b = b_mat>max_b
 cond_mc = mc_mat<min_mc
 b_mat[cond_b+cond_mc] = np.nan
 mc_mat[cond_b+cond_mc] = np.nan
+a_mat[cond_b+cond_mc] = np.nan
+db_mat[cond_b+cond_mc] = np.nan
 
-# plot b mapping
+def plot_map(d_mat, d_rng, sub_name):
+    ax = plt.gca()
+    im = plt.imshow(d_mat, cmap=cmap, origin='lower', 
+                    vmin=d_rng[0], vmax=d_rng[1],
+                    extent=[lon[0]-xy_grid/2, lon[-1]+xy_grid/2,
+                            lat[0]-xy_grid/2, lat[-1]+xy_grid/2])
+    cbar = plt.colorbar(im, ax=ax, fraction=cbar_frac, aspect=cbar_asp, pad=cbar_pad)
+    # plot faults and events
+    for fault in faults:
+        plt.plot(fault[:,0], fault[:,1], color='k', linewidth=line_wid)
+    plt.scatter(events['lon'], events['lat'], mag, 
+                color='gray', alpha=alpha, zorder=len(faults)+3)
+    # tune fig
+    cbar.ax.set_ylabel(sub_name, fontsize=fsize_label)
+    plt.setp(ax.xaxis.get_majorticklabels(), fontsize=fsize_label)
+    plt.setp(ax.yaxis.get_majorticklabels(), fontsize=fsize_label)
+    plt.title('%s Map'%sub_name, fontsize=fsize_title)
+
 fig = plt.figure(figsize=fig_size)
-ax = plt.gca()
-im = plt.imshow(b_mat, cmap=cmap, origin='lower', vmin=b_rng[0], vmax=b_rng[1],
-                extent=[lon[0]-xy_grid/2, lon[-1]+xy_grid/2,
-                        lat[0]-xy_grid/2, lat[-1]+xy_grid/2])
-cbar = plt.colorbar(im, ax=ax, fraction=cbar_frac, aspect=cbar_asp)
-# plot faults and events
-for fault in faults: plt.plot(fault[:,0], fault[:,1], color='k', linewidth=line_wid)
-plt.scatter(events['lon'], events['lat'], mag, color='gray', alpha=alpha)
-# tune fig
-cbar.ax.set_ylabel('b-value', fontsize=fsize_label)
-plt.setp(ax.xaxis.get_majorticklabels(), fontsize=fsize_label)
-plt.setp(ax.yaxis.get_majorticklabels(), fontsize=fsize_label)
-plt.title('b-Value Map: %s'%fig_title, fontsize=fsize_title)
+plt.subplot(2,2,1)
+plot_map(b_mat, b_rng, 'b-Value')
+plt.annotate('$R_{slice} = %s\degree$ \n$a_{min} = %s$'%(slice_radius, min_num), 
+             (lon[0],lat[0]), fontsize=fsize_label, fontweight='bold')
+plt.subplot(2,2,2)
+plot_map(db_mat, [None,None], 'b-Uncertainty')
+plt.subplot(2,2,3)
+plot_map(mc_mat, mc_rng, 'Mc')
+plt.subplot(2,2,4)
+plot_map(a_mat, [None,None], 'a-Value')
+plt.suptitle(fig_title, fontsize=fsize_title+2)
 plt.tight_layout()
 plt.savefig('%s/b_%s'%(fig_root, fig_fname))
-
-# plot mc mapping
-fig = plt.figure(figsize=fig_size)
-ax = plt.gca()
-im = plt.imshow(mc_mat, cmap=cmap, origin='lower', vmin=mc_rng[0], vmax=mc_rng[1],
-                extent=[lon[0]-xy_grid/2, lon[-1]+xy_grid/2,
-                        lat[0]-xy_grid/2, lat[-1]+xy_grid/2])
-cbar = plt.colorbar(im, ax=ax, fraction=cbar_frac, aspect=cbar_asp)
-# plot faults and events
-for fault in faults: plt.plot(fault[:,0], fault[:,1], color='k', linewidth=line_wid)
-plt.scatter(events['lon'], events['lat'], mag, color='gray', alpha=alpha)
-# tune fig
-cbar.ax.set_ylabel('Mc', fontsize=fsize_label)
-plt.setp(ax.xaxis.get_majorticklabels(), fontsize=fsize_label)
-plt.setp(ax.yaxis.get_majorticklabels(), fontsize=fsize_label)
-plt.title('Mc Map: %s'%fig_title, fontsize=fsize_title)
-plt.tight_layout()
-plt.savefig('%s/mc_%s'%(fig_root, fig_fname))
 plt.show()
