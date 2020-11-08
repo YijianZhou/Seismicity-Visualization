b0a6c2fe8dda8def4893bff7079929d8b6013459
Yijian ZHOU <zhouyj.pku@gmail.com>
2020-11-08 12:07:49 +0800

    Update plot_mag_time.py

--- Python/plot_mag_time.py
+++ Python/plot_mag_time.py
@@ -15,9 +15,8 @@ fout = 'output/example_mag_time.pdf'
 lon_rng = [102.2, 102.35]
 lat_rng = [29.125, 29.275]
 dep_rng = [5, 15]
+mag_rng = [-1, 4.2] # for selection
 mag_corr = 1.
-dep_corr = 0
-mag_rng = [-1, 4.2]
 # fig params
 alpha = 0.6
 mark_size = 8
@@ -28,7 +27,7 @@ fsize_title= 18
 # read catalog
 events = read_ctlg(fctlg)
 events = slice_ctlg(events, lat_rng=lat_rng, lon_rng=lon_rng, dep_rng=dep_rng, mag_rng=mag_rng)
-mag = np.array(list(events['mag']))
+mag = np.array(list(events['mag'])) + mag_corr
 ot = [oti.datetime for oti in events['ot']]
 
 # start plotting
@@ -36,7 +35,7 @@ plt.figure(figsize=fig_size)
 ax=plt.gca()
 plt.scatter(ot, mag, mark_size*np.ones(len(mag)), alpha=alpha)
 # fill edge
-plt.scatter(ot[0:2], mag_rng, alpha=0)
+plt.scatter(ot[0:2], np.array(mag_rng)+mag_corr, alpha=0)
 # plot title & label
 plt.setp(ax.xaxis.get_majorticklabels(), fontsize=fsize_label, rotation=20)
 plt.setp(ax.yaxis.get_majorticklabels(), fontsize=fsize_label)
