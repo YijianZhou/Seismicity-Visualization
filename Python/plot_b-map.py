73a5e6ed8752b3088fa862aa233bbd1661daa768
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-04-22 23:49:49 -0700

    b map

--- Python/plot_b-map.py
+++ Python/plot_b-map.py
@@ -10,40 +10,50 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # i/o paths
-fctlg = 'input/fctlg_eg.csv'
+fctlg = 'input/fctlg_eg1.csv'
 ffault = 'input/faults_eg.dat'
 fout = 'output/eg_b-map.pdf'
 titles = ['(a) b-Value', '(b) b-Uncertainty', '(c) Mc']
 # slicing criteria
-lat_rng = [24.4, 26.8]
-lon_rng = [102.5, 103.9]
-dep_rng = [0, 30]
+ot_rng = '20190704-20190710'
+ot_rng = [UTCDateTime(date) for date in ot_rng.split('-')]
+lon_rng =  [-117.82, -117.27]
+lat_rng = [35.45, 36.05]
+dep_rng = [0, 15]
+mag_rng = [-1,8]
 # b-value calc params
-xy_grid = 0.1
-slice_rad = 0.2
+xy_grid = 0.04
+slice_rad = 0.04
 min_num = 200
-b_rng = [0.8, 2.4]
-mc_rng = [None, None]
+b_rng = [0.3,1.] # if instable b estimation
 # fig config
-fig_size = (18, 11)
+fig_size = (16, 8)
 fsize_label = 14
 fsize_title = 18
 mag_corr = 0.5
-marker_size = 5.
+marker_size = 3.
 cmap = plt.get_cmap('coolwarm')
 alpha = 0.4
-cbar_frac = 0.03
-cbar_asp = 15
-cbar_pad = 0.04
+cbar_frac = 0.035 # size
+cbar_asp = 15 # shape
+cbar_pad = 0.06 # loc
 line_wid = 1. # fault trace
 
 # read catalog
 events = read_fctlg_np(fctlg)
-events = slice_ctlg(events, lat_rng=lat_rng, lon_rng=lon_rng, dep_rng=dep_rng)
-mag = (np.array(list(events['mag'])) + mag_corr) * marker_size
+events = slice_ctlg(events, ot_rng=ot_rng, lat_rng=lat_rng, lon_rng=lon_rng, dep_rng=dep_rng, mag_rng=mag_rng)
 faults = read_fault(ffault, lat_rng, lon_rng)
 
 # calc b map
+def calc_fmd(mag):
+    mag = mag[mag!=-np.inf]
+    mag_max = np.ceil(10 * max(mag)) / 10
+    mag_min = np.floor(10 * min(mag)) / 10
+    mag_bin = np.around(np.arange(mag_min-0.1, mag_max+0.2, 0.1),1)
+    num = np.histogram(mag, mag_bin)[0]
+    cum_num = np.cumsum(num[::-1])[::-1]
+    return mag_bin[1:], num, cum_num
+
 def calc_mc_maxc(mag):
     mag_bin, num, _ = calc_fmd(mag)
     return mag_bin[np.argmax(num)]
@@ -73,13 +83,11 @@ def slice_calc_mc_b(events, lat_i, lon_j):
 
 lat = np.arange(lat_rng[0], lat_rng[1], xy_grid)
 lon = np.arange(lon_rng[0], lon_rng[1], xy_grid)
-print('lat range:', round(lat[0],3), round(lat[1],3))
-print('lon range:', round(lon[0],3), round(lon[1],3))
 b_mat = -np.ones([len(lat), len(lon)])
 b_dev_mat = -np.ones([len(lat), len(lon)])
 a_mat = -np.ones([len(lat), len(lon)])
 mc_mat = -np.ones([len(lat), len(lon)])
-
+cos_lat = np.cos(np.mean(lat)*np.pi/180)
 for i,lat_i in enumerate(lat):
   for j,lon_j in enumerate(lon):
     mc_mat[i,j], b_mat[i,j], b_dev_mat[i,j], a_mat[i,j] = slice_calc_mc_b(events, lat_i, lon_j)
@@ -99,29 +107,30 @@ def plot_label(xlabel=None, ylabel=None, title=None, yvis=True):
     plt.setp(ax.xaxis.get_majorticklabels(), fontsize=fsize_label)
     plt.setp(ax.yaxis.get_majorticklabels(), fontsize=fsize_label, visible=yvis)
 
-def plot_map(d_mat, d_rng):
+def plot_map(d_mat):
     ax = plt.gca()
     im = plt.imshow(d_mat, cmap=cmap, origin='lower', 
-                    vmin=d_rng[0], vmax=d_rng[1],
                     extent=[lon[0]-xy_grid/2, lon[-1]+xy_grid/2,
                             lat[0]-xy_grid/2, lat[-1]+xy_grid/2])
     cbar = plt.colorbar(im, ax=ax, fraction=cbar_frac, aspect=cbar_asp, pad=cbar_pad, orientation='horizontal')
     # plot faults and events
     for fault in faults:
         plt.plot(fault[:,0], fault[:,1], color='k', linewidth=line_wid)
+    mag = (np.array(list(events['mag'])) + mag_corr) * marker_size
     plt.scatter(events['lon'], events['lat'], mag, color='gray', edgecolor='none', alpha=alpha, zorder=len(faults)+3)
+    ax.set_aspect(1/cos_lat)
     plt.setp(cbar.ax.xaxis.get_majorticklabels(), fontsize=fsize_label)
 
 fig = plt.figure(figsize=fig_size)
 plt.subplot(1,3,1)
-plot_map(b_mat, b_rng)
+plot_map(b_mat)
 plot_label(title=titles[0])
 plt.annotate('$R_{slice} = %s\degree$\n$N_{min} = %s$'%(slice_rad, min_num), (lon_rng[0],lat_rng[0]), fontsize=fsize_label)
 plt.subplot(1,3,2)
-plot_map(b_dev_mat, [None,None])
+plot_map(b_dev_mat)
 plot_label(title=titles[1], yvis=False)
 plt.subplot(1,3,3)
-plot_map(mc_mat, mc_rng)
+plot_map(mc_mat)
 plot_label(title=titles[2], yvis=False)
 # save fig
 plt.tight_layout()
