b555cadc20d4b55b6dc8d8f8d3b603bc1609a573
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-04-22 20:56:20 -0700

    update example output

--- Python/plot_dep-compare.py
+++ Python/plot_dep-compare.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('/home/zhouyj/software/data_prep')
 import numpy as np
 import matplotlib.pyplot as plt
+from obspy import UTCDateTime
 from reader import read_fctlg_np, read_fault, slice_ctlg, slice_ctlg_circle
 import warnings
 warnings.filterwarnings("ignore")
@@ -11,15 +12,18 @@ warnings.filterwarnings("ignore")
 # i/o paths
 fctlgs = ['input/fctlg_eg1.csv','input/fctlg_eg2.csv']
 names = ['Catalog 1','Catalog 2']
+zorders = [2,1]
 colors = ['tab:blue','tab:orange']
 title = 'Example Depth Comparison'
 fout = 'output/eg_dep-compare.pdf'
 # slicing criteria
-lon_rng =  [101.05,103.5]
-lat_rng = [27.1,30.25]
-dep_rng = [0, 30]
+ot_rng = '20190704-20190710'
+ot_rng = [UTCDateTime(date) for date in ot_rng.split('-')]
+lon_rng =  [-117.85, -117.25]
+lat_rng = [35.45, 36.05]
+dep_rng = [0, 15]
 mag_rng = [-1,8]
-bins = np.arange(0,30.1,3)
+bins = np.arange(0,15.1,1.)
 # fig config
 fig_size = (8,6)
 fsize_label = 14
@@ -38,14 +42,14 @@ def plot_label(xlabel=None, ylabel=None, title=None):
 deps = []
 for fctlg in fctlgs:
     events = read_fctlg_np(fctlg)
-    events = slice_ctlg(events, mag_rng=mag_rng, lat_rng=lat_rng, lon_rng=lon_rng, dep_rng=dep_rng)
+    events = slice_ctlg(events, ot_rng=ot_rng, lat_rng=lat_rng, lon_rng=lon_rng, dep_rng=dep_rng, mag_rng=mag_rng)
     deps.append(np.array(list(events['dep'])))
 
 # start plot
 plt.figure(figsize=fig_size)
 ax = plt.gca()
 for i in range(len(fctlgs)):
-    plt.hist(deps[i], bins, edgecolor='tab:gray', alpha=alpha, label=names[i])
+    plt.hist(deps[i], bins, edgecolor='tab:gray', alpha=alpha, zorder=zorders[i], label=names[i])
 ax.legend(fontsize=fsize_label)
 plot_label('Depth (km)', 'Number', title)
 plt.tight_layout()
