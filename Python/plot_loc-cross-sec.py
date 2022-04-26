da68218511dd0be37874ad870bd8ecbda5bc5281
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-04-25 21:40:04 -0700

    Update plot_loc-cross-sec.py

--- Python/plot_loc-cross-sec.py
+++ Python/plot_loc-cross-sec.py
@@ -35,14 +35,14 @@ col_spans = [3,3] + [1]*num_sub
 row_spans = [3,1] + [1]*num_sub
 subplot_rect = {'left':0.08, 'right':0.96, 'bottom':0.03, 'top':0.96, 'wspace':0., 'hspace':0.}
 # cross-sec config
-main_pnt = np.array([[-117.75,35.94],[-117.34,35.54]])
-main_wid = 8/111 # degree
+main_pnt = np.array([[-117.75,35.94],[-117.34,35.54]]) # northern point first
+main_W = 8/111 # degree
 main_name = ["O","O'"]
 main_color = 'tab:blue'
 sub_len = 14/111 # degree
 sub_names = ["A","A'","B","B'","C","C'","D","D'","E","E'","F","F'"]
 sub_color = 'tab:green'
-# markers in each subplots
+# in each subplot
 alpha = 0.6
 cmap = plt.get_cmap('hot')
 plt_style = ['ggplot',None][1]
@@ -61,21 +61,23 @@ fsize_title = 18
 def polar2xy(prof_polar):
     prof_xy = []
     for [lon0, lat0, theta] in prof_polar:
-        lon1 = lon0 - np.cos(theta*np.pi/180) * sub_len/2
-        lon2 = lon0 + np.cos(theta*np.pi/180) * sub_len/2
-        lat1 = lat0 - np.sin(theta*np.pi/180) * sub_len/2
-        lat2 = lat0 + np.sin(theta*np.pi/180) * sub_len/2
+        lon1 = lon0 - np.cos(theta) * sub_len/2
+        lon2 = lon0 + np.cos(theta) * sub_len/2
+        lat1 = lat0 - np.sin(theta) * sub_len/2
+        lat2 = lat0 + np.sin(theta) * sub_len/2
         prof_xy += [[lon1,lat1],[lon2,lat2]]
     return np.array(prof_xy)
 
-sub_theta = abs(np.arctan((main_pnt[0,0]-main_pnt[1,0])/(main_pnt[0,1]-main_pnt[1,1]))) * 180/np.pi
+theta = abs(np.arctan((main_pnt[0,0]-main_pnt[1,0])/(main_pnt[0,1]-main_pnt[1,1]))) # rad
 lon_step = (main_pnt[1][0] - main_pnt[0][0])/num_sub
 lat_step = (main_pnt[1][1] - main_pnt[0][1])/num_sub
 sub_pnts_polar = np.array([\
     [main_pnt[0][0]+(i+0.5)*lon_step,
-     main_pnt[0][1]+(i+0.5)*lat_step, sub_theta] for i in range(num_sub)])
+     main_pnt[0][1]+(i+0.5)*lat_step, theta] for i in range(num_sub)])
 sub_wid = ((lon_step*cos_lat)**2 + lat_step**2)**0.5 /2 # degree
+sub_W = (lon_step**2 + lat_step**2)**0.5 /2 # degree
 sub_pnts = polar2xy(sub_pnts_polar)
+main_wid = ((main_W*np.cos(theta)*cos_lat)**2 + (main_W*np.sin(theta))**2)**0.5 # real len
 
 # read catalog
 events = read_fctlg_np(fctlg)
@@ -111,25 +113,23 @@ def calc_prof(prof_pnt, prof_wid):
         prof_mag.append(mag[i])
     return prof_dist, prof_dep, prof_mag, abs_ab*111
 
-def plot_rect(prof_pnts, prof_names, prof_wid, color):
+def plot_rect(prof_pnts, prof_names, prof_W, color, ax):
+    tr0 = ax.transData
     num_prof = int(len(prof_pnts)/2)
     for ii in range(num_prof):
         # ref rect
-        prof_pnt = prof_pnts[ii*2:ii*2+2]
-        W = prof_wid
-        L = calc_dist(prof_pnt[0], prof_pnt[1])
-        theta = np.rad2deg(np.arctan((prof_pnt[1][1]-prof_pnt[0][1]) / (prof_pnt[1][0]-prof_pnt[0][0])))
-        x0, y0 = np.mean(prof_pnt[:,0]), np.mean(prof_pnt[:,1])
+        [[x1,y1],[x2,y2]] = prof_pnts[ii*2:ii*2+2]
+        prof_L = calc_dist([x1,y1],[x2,y2])
+        theta = np.rad2deg(np.arctan((y2-y1)/(x2-x1)))
+        x0, y0 =(x1+x2)/2, (y1+y2)/2
         tr = mpl.transforms.Affine2D().rotate_deg_around(x0, y0, theta) + tr0
-        rect = pat.Rectangle((x0-L/2, y0-W),L,2*W, linewidth=1.5, ls='--', edgecolor=color, facecolor='none', transform=tr)
+        rect = pat.Rectangle((x0-prof_L/2, y0-prof_W),prof_L,2*prof_W, linewidth=1.5, ls='--', edgecolor=color, facecolor='none', transform=tr)
         ax.add_patch(rect)
         # ref point name
-        for jj in range(2):
-            name = prof_names[ii*2+jj]
-            x, y = prof_pnt[jj]
-            ha = 'right' if jj%2==0 else 'left'
-            plt.annotate(name, (x,y), fontsize=fsize_label, va='center', ha=ha)
-            plt.scatter([x],[y],[ref_pnt_size], 'k')
+        name1, name2 = prof_names[ii*2:ii*2+2]
+        plt.scatter([x1,x2],[y1,y2],2*[ref_pnt_size], 'k')
+        plt.annotate(name1,(x1,y1), fontsize=fsize_label, va='center', ha='left')
+        plt.annotate(name2,(x2,y2), fontsize=fsize_label, va='center', ha='right')
 
 def plot_prof(prof_name, prof_color):
     edgex = [0,0,abs_ab,abs_ab]
@@ -153,7 +153,6 @@ fig = plt.figure(figsize=fig_size)
 # 1. plot map view
 ax = plt.subplot2grid(subplot_grids[0], org_grids[0], colspan=col_spans[0], rowspan=row_spans[0])
 ax.set_facecolor(bg_color)
-tr0 = ax.transData
 edgex = [lon_rng[0], lon_rng[0], lon_rng[1], lon_rng[1]]
 edgey = [lat_rng[0], lat_rng[1], lat_rng[0], lat_rng[1]] # fill up edge
 plt.scatter(edgex, edgey, alpha=0)
@@ -166,9 +165,9 @@ color = [cmap(1-(di-dep_rng[0])/(dep_rng[1]-dep_rng[0])) for di in dep]
 plt.scatter(lon, lat, mag, alpha=alpha, color=color, edgecolor='none', zorder=2)
 plt.grid(True, color=grid_color)
 ax.set_aspect(1/cos_lat)
-plot_rect(main_pnt, main_name, main_wid, main_color)
-plot_rect(sub_pnts, sub_names, sub_wid, sub_color)
 plot_label(title=title)
+plot_rect(main_pnt, main_name, main_W, main_color, ax)
+plot_rect(sub_pnts, sub_names, sub_W, sub_color, ax)
 # plot colorbar
 cbar_ax = fig.add_axes(cbar_pos)
 cbar = mpl.colorbar.ColorbarBase(cbar_ax, cmap=cmap)
@@ -197,7 +196,7 @@ for i in range(num_prof-1,-1,-1):
     plot_prof(sub_names[i*2:i*2+2], sub_color)
     ax.set_aspect(1)
     xvis = False if i<num_prof-1 else True
-    plot_label(xvis=xvis)
+    plot_label(None, 'Depth (km)', xvis=xvis)
     plt.ylabel('Depth (km)', rotation=-90, va='bottom', fontsize=fsize_label)
 # save fig
 plt.subplots_adjust(**subplot_rect)
