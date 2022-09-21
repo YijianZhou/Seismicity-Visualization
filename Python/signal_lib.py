06f95bb2dcf8a0ba41047552d4d257bd1f53b1f9
Yijian ZHOU <34815814+YijianZhou@users.noreply.github.com>
2022-09-20 19:24:22 -0700

    Update signal_lib.py

--- Python/signal_lib.py
+++ Python/signal_lib.py
@@ -13,9 +13,9 @@ def preprocess(stream, samp_rate, freq_band):
     # resample data
     org_rate = st[0].stats.sampling_rate
     if org_rate!=samp_rate: st = st.interpolate(samp_rate)
-    for ii in range(3):
-        st[ii].data[np.isnan(st[ii].data)] = 0
-        st[ii].data[np.isinf(st[ii].data)] = 0
+    for tr in st:
+        tr.data[np.isnan(tr.data)] = 0
+        tr.data[np.isinf(tr.data)] = 0
     # filter
     st = st.detrend('demean').detrend('linear').taper(max_percentage=0.05, max_length=10.)
     freq_min, freq_max = freq_band
